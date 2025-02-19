4f107250891dba472085e68f857f496966272536
Jose Castro <josecastropuertollano@gmail.com>
2025-02-19 09:01:18 +0100

    fix 01_16 exercise

--- Chapter_01/01_16_Exercise.c
+++ Chapter_01/01_16_Exercise.c
@@ -8,6 +8,8 @@ void copy(char to[], char from[]);
     Revise the main routine of the longest-line program so it will correctly print the length of arbitrarily long input lines, 
     and as much as possible of the text.
 */
+
+/* print longest input line*/
 main()
 {
     int len;            /* current line length */
@@ -16,30 +18,39 @@ main()
     char longest[MAXLINE];  /* longest line saved here */
 
     max = 0;
-    while ((len = getLine(line, MAXLINE)) > 0)
+    
+    while ((len = getLine(line, MAXLINE)) > 0) {
+        printf("\n%s -- length: %d\n", line, len - 1);
         if (len > max) {
             max = len;
             copy(longest, line);
         }
-        if (max > 0)    /* there was a line */
-            printf("%s", longest);
-        return 0;
+    }
+    if (max > 0)    /* there was a line */
+        printf("%s", longest);
+    return 0;
 }
 
-/* getline: read a line into s, return length */
+/* getline: read a line into s, return length       */
 int getLine(char s[], int lim) {
-    int c, i;
+    int c, i, j;
 
-    for (i=0; i<lim-1 && (c=getchar()) !=EOF && c !='\n'; ++i)
-        s[i] = c;
+    j = 0;
+    for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i)
+        if (i < lim - 2) {
+            s[j] = c;   /* line still in boundaries */
+            ++j;
+        }
     if (c == '\n') {
-        s[i] = c;
+        s[j] = c;
+        ++j;
         ++i;
     }
-    s[i] = '/0';
+    s[j] = '\0';
     return i;
 }
 
+/* copy: copu 'from' into 'to'; assume to is big enough */
 void copy(char to[], char from[]) {
     int i;
 
