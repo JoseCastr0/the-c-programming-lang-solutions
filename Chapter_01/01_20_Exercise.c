4ab2ae78ed975239cd43a75e1c098c8b9475c4ec
Jose Castro <josecastropuertollano@gmail.com>
2025-02-28 11:56:18 +0100

    feat: start exercise 30

---
+++ Chapter_01/01_20_Exercise.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+    Write a program detab that replaces tabs in the input with the proper number of blanks to space 
+    to the next tab stop. Assume a fixed set of tab stops, say every n columns. Should n be a 
+    variable or a symbolic parameter?
+*/
+int main()
+{
+    int c, i, j, n;
+    n = 4; /*  number of spaces to replace a tab */
+    while ((c = getchar()) != EOF) {
+
+    }
+}
+
