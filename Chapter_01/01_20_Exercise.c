09c9a4e0bcc5510eb380c836b1d8a6ddece68207
Jose Castro <josecastropuertollano@gmail.com>
2025-03-04 07:58:58 +0100

    add exercise 20

--- Chapter_01/01_20_Exercise.c
+++ Chapter_01/01_20_Exercise.c
@@ -4,12 +4,27 @@
     to the next tab stop. Assume a fixed set of tab stops, say every n columns. Should n be a 
     variable or a symbolic parameter?
 */
+#define TABINC 4        /* tab increment size               */
 int main()
 {
-    int c, i, j, n;
-    n = 4; /*  number of spaces to replace a tab */
+    int c, nb, pos;
+    nb = 0;             /* number of blanks necessary       */
+    pos = 1;            /* position of character in line    */
     while ((c = getchar()) != EOF) {
-
+        if (c == '\t') {
+            nb = TABINC - (pos - 1) % TABINC;
+            while (nb > 0) {
+                putchar(' ');
+                ++pos;
+                --nb;
+            }
+        } else if (c == '\n') { /* newline character        */
+            putchar(c);
+            pos = 1;
+        } else {                /* all other characters     */
+            putchar(c);
+            ++pos;
+        }
     }
 }
 
